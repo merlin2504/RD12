364d12fdc3efbe8b1feaeb1271f39cd7b2884175
merlin2504 <maziz@ymail.com>
2011-04-24 01:34:09 -0500

    Service Configuration

--- PACT.MODEL/Screens/ControlGenerator.cs
+++ PACT.MODEL/Screens/ControlGenerator.cs
@@ -10,6 +10,7 @@ using System.Windows.Input;
 using System.ServiceModel;
 using System.Data;
 using System.IO;
+using System.Configuration;
 
 namespace PACT.MODEL
 {
@@ -17,97 +18,99 @@ namespace PACT.MODEL
     {
         private ObservableCollection<PactControlData> _PactControlData;
 
-        private XmlDocument GetScreenInfo(int _ScreenID,string _CompanyIndex)
+        private XmlDocument GetScreenInfo(int _ScreenID, string _CompanyIndex)
         {
             XmlDocument xDoc = new XmlDocument();
-            CommonService.CommonClient wcfService = null ;
-            try{
-                //var endpoint = new EndpointAddress(new Uri(_ServiceUrl+"Common.svc"));
-                //var binding = new WSHttpBinding();
-                //wcfService= new CommonService.CommonClient(binding, endpoint);
-                wcfService= new CommonService.CommonClient();
+            CommonService.CommonClient wcfService = null;
+            try
+            {
+
+                var endpoint = new EndpointAddress(new Uri(ConfigurationManager.AppSettings["SERVICEURL"].ToString()));
+                var binding = new WSHttpBinding();
+                wcfService = new CommonService.CommonClient(binding, endpoint);
+                //wcfService= new CommonService.CommonClient();
                 wcfService.Open();
-                DataSet ds = wcfService.GetScreenInfoByID(_ScreenID,_CompanyIndex);
-                if(ds!=null && ds.Tables.Count>0)
+                DataSet ds = wcfService.GetScreenInfoByID(_ScreenID, _CompanyIndex);
+                if (ds != null && ds.Tables.Count > 0)
                 {
 
                     xDoc.LoadXml(ds.Tables[0].Rows[0]["ScreenXML"].ToString());
                 }
                 return xDoc;
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                throw new Exception("Error in ControlGenerator::GetScreenInfo::-->"+ex.StackTrace);
+                throw new Exception("Error in ControlGenerator::GetScreenInfo::-->" + ex.StackTrace);
             }
             finally
             {
-                if(wcfService!=null)
+                if (wcfService != null)
                 {
                     wcfService.Close();
                 }
-            
+
             }
         }
 
-        public ObservableCollection<PactControlData> GetControls(string ScreenID,string CompanyIndex)
+        public ObservableCollection<PactControlData> GetControls(string ScreenID, string CompanyIndex)
         {
-                if (_PactControlData == null)
+            if (_PactControlData == null)
+            {
+                string strID = "";
+                string strLable = "";
+                string strVal = "";
+                string strNm = "";
+                XmlDocument xDoc = GetScreenInfo(Convert.ToInt32(ScreenID), CompanyIndex);
+                _PactControlData = new ObservableCollection<PactControlData>();
+                for (int iRow = 0; iRow < xDoc.DocumentElement.ChildNodes.Count; iRow++)
                 {
-                    string strID = "";
-                    string strLable = "";
-                    string strVal = "";
-                    string strNm = "";
-                    XmlDocument xDoc = GetScreenInfo(Convert.ToInt32(ScreenID),CompanyIndex);
-                    _PactControlData = new ObservableCollection<PactControlData>();
-                    for (int iRow = 0; iRow < xDoc.DocumentElement.ChildNodes.Count; iRow++)
+                    XmlNode xSec = xDoc.DocumentElement.ChildNodes[iRow];
+                    foreach (XmlNode xRow in xSec.ChildNodes)
                     {
-                        XmlNode xSec = xDoc.DocumentElement.ChildNodes[iRow];
-                        foreach (XmlNode xRow in xSec.ChildNodes)
+                        foreach (XmlNode xTemp in xRow.ChildNodes)
                         {
-                            foreach (XmlNode xTemp in xRow.ChildNodes)
+                            foreach (XmlNode xRowInfo in xTemp.ChildNodes)
                             {
-                                foreach (XmlNode xRowInfo in xTemp.ChildNodes)
+                                strID = xRowInfo.SelectSingleNode("ID").InnerText.ToString();
+                                strLable = xRowInfo.SelectSingleNode("Label").InnerText.ToString();
+                                strNm = xRowInfo.SelectSingleNode("Control").InnerText.ToString();
+                                switch (strNm)
                                 {
-                                    strID = xRowInfo.SelectSingleNode("ID").InnerText.ToString();
-                                    strLable = xRowInfo.SelectSingleNode("Label").InnerText.ToString();
-                                    strNm = xRowInfo.SelectSingleNode("Control").InnerText.ToString();
-                                    switch (strNm)
-                                    {
-                                        case "TextBox":
-                                            _PactControlData.Add(new PactTextBlockData()
-                                            {
-                                                Text = strLable,
-                                                Style = "PACTTextBlockStyle"
-                                            });
-                                            _PactControlData.Add(new PactTextBoxData()
-                                            {
-                                                Text = strID,
-                                            });
-                                            break;
+                                    case "TextBox":
+                                        _PactControlData.Add(new PactTextBlockData()
+                                        {
+                                            Text = strLable,
+                                            Style = "PACTTextBlockStyle"
+                                        });
+                                        _PactControlData.Add(new PactTextBoxData()
+                                        {
+                                            Text = strID,
+                                        });
+                                        break;
 
-                                        case "ComboBox":
-                                            _PactControlData.Add(new PactTextBlockData()
-                                            {
-                                                Text = strLable,
-                                            });
-                                            PactComboBoxData CMB = new PactComboBoxData();
-                                            CMB.ComboItems.Add(strID);
-                                            _PactControlData.Add(CMB);
-                                            break;
+                                    case "ComboBox":
+                                        _PactControlData.Add(new PactTextBlockData()
+                                        {
+                                            Text = strLable,
+                                        });
+                                        PactComboBoxData CMB = new PactComboBoxData();
+                                        CMB.ComboItems.Add(strID);
+                                        _PactControlData.Add(CMB);
+                                        break;
 
-                                        case "Button":
-                                            _PactControlData.Add(new PactButtonData()
-                                            {
-                                                Label = strLable,
-                                                DynamicCommand = "OnSave",
-                                            });
-                                            break;
-                                    }
+                                    case "Button":
+                                        _PactControlData.Add(new PactButtonData()
+                                        {
+                                            Label = strLable,
+                                            DynamicCommand = "OnSave",
+                                        });
+                                        break;
                                 }
                             }
                         }
                     }
-                }   
+                }
+            }
             return _PactControlData;
         }
     }
